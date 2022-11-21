a319bdaedc2ed0c90b5e94fafd2f6f33d2d5e13b
Siimy-Mo <qq1074306467@gmail.com>
2022-11-21 18:02:10 +0800

    Add: add a table outlines in staffInterface

--- pages/timePill/staffInterface/StaffInterfaceView.tsx
+++ pages/timePill/staffInterface/StaffInterfaceView.tsx
@@ -11,7 +11,7 @@ interface StaffInterfaceViewProps {
 export default function StaffInterfaceView(props: StaffInterfaceViewProps) {
     // export default function LoginView() {
     const { handleSignIn, } = props;
-    const [register, setRegister] = useState(false)
+    const [PillLabel, setPillLabel] = useState(0)
     const router = useRouter();
 
     return (
@@ -25,19 +25,246 @@ export default function StaffInterfaceView(props: StaffInterfaceViewProps) {
                             staff操作界面</h3>
                     </div>
 
-                    <div className='flex flex-col items-center w-full px-12 py-6 pt-12 rounded-xl bg-red-300 timePillIntroShadowOn'>
-                        功能界面劃分：
-                        <div >現有膠囊狀態數</div>
-                        <div>
-                            未發送膠囊(點擊後下拉) -> 手動發送郵件
+                    <div className='flex flex-col items-center w-96 px-12 py-6 pt-12 rounded-xl bg-red-300 timePillIntroShadowOn'>
+                        胶囊状态：
+                        <div >
+                            <div className="overflow-x-auto relative">
+                                <table className="w-full text-sm text-left  ">
+                                    <thead className="text-xs text-gray-900 uppercase dark:text-gray-400">
+                                        <tr>
+                                            <th scope="col" className="py-3 px-6">
+                                                未到期
+                                            </th>
+                                            <th scope="col" className="py-3 px-6">
+                                                已到期
+                                            </th>
+                                            <th scope="col" className="py-3 px-6">
+                                                已完成
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr className="">
+                                            <td className="py-2 px-6">
+                                                20
+                                            </td>
+                                            <td className="py-2 px-6">
+                                                5
+                                            </td>
+                                            <td className="py-2 px-6">
+                                                10
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
                         </div>
-                        <div>
-                            取出膠囊
+
+                        {/* 展示界面： */}
+                        <div className='h-fit' >
+                            未到期胶囊界面
+                            <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+                                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                        <tr>
+                                            <th scope="col" className="p-4">
+                                                <div className="flex items-center">
+                                                    <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                                    <label htmlFor="checkbox-all-search" className="sr-only">checkbox</label>
+                                                </div>
+                                            </th>
+                                            <th scope="col" className="py-3 px-6">
+                                                胶囊编号
+                                            </th>
+                                            <th scope="col" className="py-3 px-6">
+                                                到期日期
+                                            </th>
+
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                            <td className="p-4 w-4">
+                                                <div className="flex items-center">
+                                                    <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                                    <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
+                                                </div>
+                                            </td>
+                                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                123456
+                                            </th>
+                                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                12/21
+                                            </th>
+
+                                        </tr>
+                                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                            <td className="p-4 w-4">
+                                                <div className="flex items-center">
+                                                    <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                                    <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
+                                                </div>
+                                            </td>
+                                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                654321
+                                            </th>
+                                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                12/21
+                                            </th>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div className='flex w-full justify-between'>
+
+                                <button onClick={() => setPillLabel(1)}>提前领取胶囊</button>
+                            </div>
                         </div>
+
+                        {/* 展示界面： */}
+                        <div className='h-fit' >
+                            已到期胶囊界面
+                            <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+                                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                        <tr>
+                                            <th scope="col" className="p-4">
+                                                <div className="flex items-center">
+                                                    <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                                    <label htmlFor="checkbox-all-search" className="sr-only">checkbox</label>
+                                                </div>
+                                            </th>
+                                            <th scope="col" className="py-3 px-6">
+                                                胶囊编号
+                                            </th>
+                                            <th scope="col" className="py-3 px-6">
+                                                到期日期
+                                            </th>
+                                            <th scope="col" className="py-3 px-6">
+                                                是否通知
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                            <td className="p-4 w-4">
+                                                <div className="flex items-center">
+                                                    <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                                    <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
+                                                </div>
+                                            </td>
+                                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                123456
+                                            </th>
+                                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                12/21
+                                            </th>
+                                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                未通知
+                                            </th>
+                                        </tr>
+                                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                            <td className="p-4 w-4">
+                                                <div className="flex items-center">
+                                                    <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                                    <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
+                                                </div>
+                                            </td>
+                                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                654321
+                                            </th>
+                                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                12/21
+                                            </th>
+                                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                已通知
+                                            </th>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div className='flex w-full justify-between'>
+
+                                <button onClick={() => setPillLabel(0)}>发送邮件</button>
+                                <button onClick={() => setPillLabel(1)}>领取胶囊</button>
+                                <button onClick={() => setPillLabel(2)}>设置过期</button>
+                            </div>
+                        </div>
+
+                        {/* 展示界面： */}
+                        <div className='h-fit' >
+                            已完成胶囊界面
+                            <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+                                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                        <tr>
+                                            <th scope="col" className="p-4">
+                                                <div className="flex items-center">
+                                                    <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                                    <label htmlFor="checkbox-all-search" className="sr-only">checkbox</label>
+                                                </div>
+                                            </th>
+                                            <th scope="col" className="py-3 px-6">
+                                                胶囊编号
+                                            </th>
+                                            <th scope="col" className="py-3 px-6">
+                                                到期日期
+                                            </th>
+                                            <th scope="col" className="py-3 px-6">
+                                                完成方式
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                            <td className="p-4 w-4">
+                                                <div className="flex items-center">
+                                                    <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                                    <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
+                                                </div>
+                                            </td>
+                                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                123456
+                                            </th>
+                                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                12/21
+                                            </th>
+                                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                过期
+                                            </th>
+                                        </tr>
+                                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                            <td className="p-4 w-4">
+                                                <div className="flex items-center">
+                                                    <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                                    <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
+                                                </div>
+                                            </td>
+                                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                654321
+                                            </th>
+                                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                12/21
+                                            </th>
+                                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                已领取
+                                            </th>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div className='flex w-full justify-between'>
+
+                                <button onClick={() => setPillLabel(0)}>取消领取？</button>
+                            </div>
+                        </div>
+
+
+
 
                     </div>
                 </div>
-                {/* 确认登入之后显示query和update的选项！ */}
 
             </div>
 
