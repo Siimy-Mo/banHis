66c25541d58cadea11079deec9151a9d3b6d1814
Siimy-Mo <qq1074306467@gmail.com>
2022-12-01 02:42:59 +0800

    Bug修復: map裡不return

--- components/common/PillStatusTables.tsx
+++ components/common/PillStatusTables.tsx
@@ -68,24 +68,23 @@ const table0 = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {dataset1.map((row, index) => {
-                            return (
-                                <tr key={row.number} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                    <td className="p-4 w-4">
-                                        <div className="flex items-center">
-                                            <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                                            <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
-                                        </div>
-                                    </td>
-                                    <th scope="row" className="py-4 px-6 font-medium whitespace-nowrap dark:text-white">
-                                        {row.number}
-                                    </th>
-                                    <th scope="row" className="py-4 px-6 font-medium whitespace-nowrap dark:text-white">
-                                        {row.ddl}
-                                    </th>
-                                </tr>
-                            )
-                        })
+                        {dataset1.map((row, index) => (
+                            <tr key={row.number} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <td className="p-4 w-4">
+                                    <div className="flex items-center">
+                                        <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                        <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
+                                    </div>
+                                </td>
+                                <th scope="row" className="py-4 px-6 font-medium whitespace-nowrap dark:text-white">
+                                    {row.number}
+                                </th>
+                                <th scope="row" className="py-4 px-6 font-medium whitespace-nowrap dark:text-white">
+                                    {row.ddl}
+                                </th>
+                            </tr>
+                        )
+                        )
                         }
                     </tbody>
                 </table>
@@ -95,7 +94,7 @@ const table0 = () => {
             </div>
 
         </div>
-        )
+    )
 }
 
 const table1 = () => {
