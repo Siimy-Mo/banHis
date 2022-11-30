8ea01bc2af4ccdb37312826e57db3bcaca9aacf8
Siimy-Mo <qq1074306467@gmail.com>
2022-12-01 02:46:50 +0800

    Bug修復: map排查

--- components/common/PillStatusTables.tsx
+++ components/common/PillStatusTables.tsx
@@ -68,7 +68,8 @@ const table0 = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {dataset1.map((row, index) => (
+                        {/* {
+                        dataset1.map((row, index) => (
                             <tr key={row.number} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                 <td className="p-4 w-4">
                                     <div className="flex items-center">
@@ -83,9 +84,7 @@ const table0 = () => {
                                     {row.ddl}
                                 </th>
                             </tr>
-                        )
-                        )
-                        }
+                        ))} */}
                     </tbody>
                 </table>
             </div>
