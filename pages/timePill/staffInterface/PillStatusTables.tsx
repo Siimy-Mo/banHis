f7278130ee6c7c235d6889fea1bdb02de7125fcf
Siimy-Mo <qq1074306467@gmail.com>
2022-12-13 18:31:08 +0800

    Update: status table modification

--- pages/timePill/staffInterface/PillStatusTables.tsx
+++ pages/timePill/staffInterface/PillStatusTables.tsx
@@ -45,7 +45,7 @@ function HeadNav(props: UploadingProps) {
         { manual: true }
     );
 
-    const getPills = async (status: Array<[[],[]]>) => {//两个promise，合并需要Promise.all来处理
+    const getPills = async (status: Array<[string,string]>) => {//两个promise，合并需要Promise.all来处理
         // console.log('getPills function ! with status:', status)
         const res = await getAllPillsWithStatus(apiSetting.PillStatus.getAllPillsWithStatus(headers, status[0]))
         const res1 = await getAllPillsWithStatus(apiSetting.PillStatus.getAllPillsWithStatus(headers, status[1]))
