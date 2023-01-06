152815d22f91bd2e13548b6d284f9d64527756a3
Siimy-Mo <qq1074306467@gmail.com>
2023-01-06 19:14:21 +0800

    Add：table分页的nav

--- pages/timePill/staffInterface/PillStatusTables.tsx
+++ pages/timePill/staffInterface/PillStatusTables.tsx
@@ -245,46 +245,81 @@ const table1 = (pillContent: any, handleChange: any) => {
 
 const table2 = (pillContent: any, handleChange: any) => {
     return (
-        <table className="w-full text-sm text-left  dark:text-gray-400">
-            <thead className="text-xs uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                    <th scope="col" className="p-4">
-                    </th>
-                    <th scope="col" className="py-3 px-6">
-                        膠囊編號
-                    </th>
-                    <th scope="col" className="py-3 px-6">
-                        到期日期
-                    </th>
-                    <th scope="col" className="py-3 px-6">
-                        完成途徑
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    pillContent.map((row: any) => (
-                        <tr key={row.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <td className="p-4 w-4">
-                                <div className="flex items-center">
-                                    <input id="default-radio-1" onChange={handleChange} type="radio" value={row.code + row.id} name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                                    <label htmlFor="default-radio-1" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"></label>
-                                </div>
-                            </td>
-                            <th scope="row" className="py-4 px-6 font-medium whitespace-nowrap dark:text-white">
-                                {row.id}
-                            </th>
-                            <th scope="row" className="py-4 px-6 font-medium whitespace-nowrap dark:text-white">
-                                {cutDate(row.deadline)}
-                            </th>
-                            <th scope="row" className="py-4 px-6 font-medium whitespace-nowrap dark:text-white">
-                                {row.status}
-                            </th>
-                        </tr>
+        <div >
+            <table className="w-full text-sm text-left  dark:text-gray-400">
+                <thead className="text-xs uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" className="p-4">
+                        </th>
+                        <th scope="col" className="py-3 px-6">
+                            膠囊編號
+                        </th>
+                        <th scope="col" className="py-3 px-6">
+                            到期日期
+                        </th>
+                        <th scope="col" className="py-3 px-6">
+                            完成途徑
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        pillContent.map((row: any) => (
+                            <tr key={row.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <td className="p-4 w-4">
+                                    <div className="flex items-center">
+                                        <input id="default-radio-1" onChange={handleChange} type="radio" value={row.code + row.id} name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                        <label htmlFor="default-radio-1" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"></label>
+                                    </div>
+                                </td>
+                                <th scope="row" className="py-4 px-6 font-medium whitespace-nowrap dark:text-white">
+                                    {row.id}
+                                </th>
+                                <th scope="row" className="py-4 px-6 font-medium whitespace-nowrap dark:text-white">
+                                    {cutDate(row.deadline)}
+                                </th>
+                                <th scope="row" className="py-4 px-6 font-medium whitespace-nowrap dark:text-white">
+                                    {row.status}
+                                </th>
+                            </tr>
 
-                    ))}
-            </tbody>
-        </table>
+                        ))}
+                </tbody>
+            </table>
+            {/* <nav className="flex items-center justify-between pt-4 px-6 bg-gray-50" aria-label="Table navigation">
+                <span className="text-sm font-normal text-gray-500 dark:text-gray-400">Showing <span className="font-semibold text-gray-900 dark:text-white">1-10</span> of <span className="font-semibold text-gray-900 dark:text-white">1000</span></span>
+                <ul className="inline-flex items-center -space-x-px">
+                    <li>
+                        <a href="#" className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                            <span className="sr-only">Previous</span>
+                            <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
+                    </li>
+                    <li>
+                        <a href="#" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+                    </li>
+                    <li>
+                        <a href="#" aria-current="page" className="z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+                    </li>
+                    <li>
+                        <a href="#" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">...</a>
+                    </li>
+                    <li>
+                        <a href="#" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">100</a>
+                    </li>
+                    <li>
+                        <a href="#" className="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                            <span className="sr-only">Next</span>
+                            <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                        </a>
+                    </li>
+                </ul>
+            </nav> */}
+
+        </div>
 
     )
 }
